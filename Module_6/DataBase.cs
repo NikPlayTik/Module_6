4e7074ca0a0903edaf66385c45fe6d2ce7a093ef
Nikita Anishenco <tanishenco91@gmail.com>
2023-10-19 11:39:13 +0300

    Приложение корректно работает

--- Module_6/DataBase.cs
+++ Module_6/DataBase.cs
@@ -29,14 +29,14 @@ namespace Module_6
         {
             try
             {
-                using (SqlCommand cmd = new SqlCommand("INSERT INTO BooksData1 (author_Name_Surname, nameBook, yearRelease, Rented) VALUES (@author, @name, @year, @rented)", sqlConnection))
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO BooksData1 (author_Name_Surname, nameBook, yearRelease, Rented) OUTPUT INSERTED.id_books VALUES (@author, @name, @year, @rented)", sqlConnection))
                 {
                     cmd.Parameters.AddWithValue("@author", authorName);
                     cmd.Parameters.AddWithValue("@name", bookName);
                     cmd.Parameters.AddWithValue("@year", yearRelease);
                     cmd.Parameters.AddWithValue("@rented", Rented);
                     sqlConnection.Open();
-                    cmd.ExecuteNonQuery();
+                    int id = (int)cmd.ExecuteScalar(); // Получить значение id_books
                 }
             }
             finally
@@ -44,19 +44,46 @@ namespace Module_6
                 closeConnection();
             }
         }
-        public List<string> GetBooks()
+        public bool RentBook(int id_books, bool isRented)
         {
-            List<string> books = new List<string>();
             try
             {
-                using (SqlCommand cmd = new SqlCommand("SELECT author_Name_Surname, nameBook, yearRelease FROM BooksData1", sqlConnection))
+                openConnection();
+
+                // Обновляем состояние аренды в базе данных в соответствии с isRented
+                using (SqlCommand rentCmd = new SqlCommand("UPDATE BooksData1 SET Rented = @isRented WHERE id_books = @id_books", sqlConnection))
+                {
+                    rentCmd.Parameters.AddWithValue("@id_books", id_books);
+                    rentCmd.Parameters.AddWithValue("@isRented", isRented);
+                    int rowsAffected = rentCmd.ExecuteNonQuery();
+
+                    return rowsAffected > 0; // Возвращаем true, если аренда успешно обновлена
+                }
+            }
+            finally
+            {
+                closeConnection();
+            }
+        }
+        public List<Book> GetBooks()
+        {
+            List<Book> books = new List<Book>();
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT id_books, author_Name_Surname, nameBook, yearRelease FROM BooksData1", sqlConnection))
                 {
                     openConnection();
                     SqlDataReader reader = cmd.ExecuteReader();
                     while (reader.Read())
                     {
-                        string bookInfo = $"{reader["author_Name_Surname"]} - {reader["nameBook"]} ({reader["yearRelease"]})";
-                        books.Add(bookInfo);
+                        Book book = new Book
+                        {
+                            Id = Convert.ToInt32(reader["id_books"]), // Установите Id книги из базы данных
+                            Author = reader["author_Name_Surname"].ToString(),
+                            Title = reader["nameBook"].ToString(),
+                            Year = Convert.ToInt32(reader["yearRelease"])
+                        };
+                        books.Add(book);
                     }
                 }
             }
--- Module_6/MainWindow.xaml.cs
+++ Module_6/MainWindow.xaml.cs
@@ -1,25 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Text.RegularExpressions;
-using System.Data.Entity;
+using System.Data.SqlClient;
 
 namespace Module_6
 {
     public partial class MainWindow : Window
     {
         DataBase database = new DataBase();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -35,7 +29,7 @@ namespace Module_6
                 {
                     int year = int.Parse(yearText);
 
-                    bool isRented = RentCheckBox.IsChecked ?? false;
+                    bool isRented = false; // Новая книга по умолчанию не арендована
                     database.AddBook(ФИО.Text, Название.Text, year, isRented);
 
                     // Очищаем текстовые поля
@@ -54,10 +48,43 @@ namespace Module_6
             }
         }
 
+        private void RentCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            CheckBox rentCheckBox = (CheckBox)sender;
+            Book book = (Book)rentCheckBox.DataContext; // Получаем книгу, связанную с CheckBox
+
+            if (rentCheckBox.IsChecked == true)
+            {
+                if (database.RentBook(book.Id, true))
+                {
+                    MessageBox.Show("Книга успешно арендована");
+                }
+                else
+                {
+                    MessageBox.Show("Книга уже арендована или произошла ошибка при аренде книги");
+                    rentCheckBox.IsChecked = false; // Отменить изменение CheckBox
+                }
+            }
+            else
+            {
+                // Если снимается аренда, вызываем метод RentBook и передаем false
+                if (database.RentBook(book.Id, false))
+                {
+                    MessageBox.Show("Аренда книги успешно снята");
+                }
+                else
+                {
+                    MessageBox.Show("Произошла ошибка при снятии аренды книги");
+                    rentCheckBox.IsChecked = true; // Восстановить галочку CheckBox
+                }
+            }
+        }
+
         private void OutputButton_Click(object sender, RoutedEventArgs e)
         {
-            List<string> books = database.GetBooks();
+            List<Book> books = database.GetBooks();
             View_information.ItemsSource = books;
         }
+
     }
 }
